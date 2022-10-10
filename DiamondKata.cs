1e0e88ab998d4b94682a057272fd9c06aec95dc8
Shah Alam Khan <alam037@gmail.com>
2022-10-10 21:16:22 +0530

    Add project files.

---
+++ DiamondKata.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiamondKataTDD
+{
+    public class DiamondKata
+    {
+        public static IEnumerable<string> Generate(char c)
+        {
+            return GenerateFinal(c);
+        }
+
+        private static IEnumerable<string> Generate1(char c)
+        {
+            yield return "A";
+        }
+
+        private static IEnumerable<string> Generate2(char c)
+        {
+            yield return " A ";
+            yield return $"{c} {c}";
+            yield return " A ";
+        }
+
+        private static IEnumerable<string> Generate3(char c)
+        {
+            if (c == 'A')
+            {
+                yield return "A";
+            }
+            else
+            {
+                yield return " A ";
+                yield return $"{c} {c}";
+                yield return " A ";
+            }
+        }
+
+        private static IEnumerable<string> GenerateFinal(char c)
+        {
+            for (var i = 'A'; i < c; i++) yield return GenerateRow(i, c);
+            for (var i = c; i >= 'A'; i--) yield return GenerateRow(i, c);
+        }
+
+        private static string GenerateRow(char currentChar, char maxChar)
+        {
+            var length = (maxChar - 'A' + 1) * 2 - 1;
+            var padding = maxChar - currentChar;
+            var sb = new StringBuilder();
+
+            sb.Append(new string(' ', padding));
+            sb.Append(currentChar);
+            if (currentChar != 'A')
+            {
+                var insidePadding = length - padding * 2 - 2;
+                sb.Append(new string(' ', insidePadding));
+                sb.Append(currentChar);
+            }
+
+            sb.Append(new string(' ', padding));
+            //Write the result into text file
+           //WriteResult(sb.ToString());
+            return sb.ToString();
+        }
+
+        public static void WriteResult(string strMessage)
+        {
+            try
+            {
+                var startDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", startDirectory, "TestResult.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage);
+                objStreamWriter.Close();
+                objFilestream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DiamondTDD.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace DiamondKataTDD
+{
+    public class DiamondTDD
+    {
+
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        private Property AlphabetError_When_NumberIsEntered(char c)
+        {
+           if (Char.IsDigit(c)) throw new Exception("Number is not allowed!");
+            else
+              return true.ToProperty();
+
+        }
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        private Property AlphabetError_When_SpecialCharacterIsEntered(char c)
+        {
+            if (!Char.IsLetterOrDigit(c)) throw new Exception("Special Character is not allowed!");
+            else
+                return true.ToProperty();
+         }
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        private Property OneAlphabetError_When_MoreThanOneAlphabetsAreEntered(string c)
+        {
+            if (c.Length >1) throw new Exception("More than one alphabet are not allowed!");
+            else
+                return true.ToProperty();
+        }
+        //Draw empty space
+         [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property NotEmpty(char c)
+        {
+            return DiamondKata.Generate(c).All(s => s != string.Empty).ToProperty();
+        }
+
+        ////First row draw char A
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property FirstLineContainsA(char c)
+        {
+            return DiamondKata.Generate(c).First().Contains('A').ToProperty();
+        }
+
+        //Last row draw char A
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property LastLineContainsA(char c)
+        {
+            return DiamondKata.Generate(c).Last().Contains('A').ToProperty();
+        }
+        //Add Spaces Per Row
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property SpacesPerRow(char c)
+        {
+            return DiamondKata.Generate(c).All(row =>
+                CountLeadingSpaces(row) == CountTrailingSpaces(row)
+            ).ToProperty();
+        }
+
+        //Rows Contain Correct Letter In Correct Order
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property GetCharInCorrectOrder(char c)
+        {
+            var expected = new List<char>();
+            for (var i = 'A'; i < c; i++) expected.Add(i);
+
+            for (var i = c; i >= 'A'; i--) expected.Add(i);
+
+            var actual = DiamondKata.Generate(c).ToList().Select(GetCharInRow);
+            return actual.SequenceEqual(expected).ToProperty();
+        }
+
+        //Draw Diamond Width Equals Height
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property DiamondWidthEqualsHeight(char c)
+        {
+            var diamond = DiamondKata.Generate(c).ToList();
+            return diamond.All(row => row.Length == diamond.Count).ToProperty();
+        }
+
+
+        //All Rows Except First And Last Contain Two Identical Letters
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property FisrtLastContaintwoIdenticalLatter(char c)
+        {
+            if (c == 'A') return true.ToProperty();
+
+            var diamond = DiamondKata.Generate(c).ToArray()[1..^1];
+            return diamond.All(x =>
+            {
+                var s = x.Replace(" ", string.Empty);
+                var b = s.Length == 2 && s.First() == s.Last();
+                return b;
+            }).ToProperty();
+        }
+
+        //Around Horizontal Axis
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property ReversebottomHalf(char c)
+        {
+            var diamond = DiamondKata.Generate(c).ToArray();
+            var half = diamond.Length / 2;
+            var topHalf = diamond[..half];
+            var bottomHalf = diamond[(half + 1)..];
+            return topHalf.Reverse().SequenceEqual(bottomHalf).ToProperty();
+        }
+
+        //Around Vertical Axis
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public Property ReversesecondHalf(char c)
+        {
+            return DiamondKata.Generate(c).ToArray()
+                .All(row =>
+                {
+                    var half = row.Length / 2;
+                    var firstHalf = row[..half];
+                    var secondHalf = row[(half + 1)..];
+
+                    return firstHalf.Reverse().SequenceEqual(secondHalf);
+                }).ToProperty();
+        }
+
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        //Input Letter Row Contains Number Outside Padding Spaces
+        public Property OutsidePaddingSpaces(char c)
+        {
+            var inputLetterRow = DiamondKata.Generate(c).ToArray().First(x => GetCharInRow(x) == c);
+            return (inputLetterRow[0] != ' ' && inputLetterRow[^1] != ' ').ToProperty();
+        }
+
+        private int CountLeadingSpaces(string s)
+        {
+            return s.IndexOf(GetCharInRow(s));
+        }
+
+        private static char GetCharInRow(string row)
+        {
+            return row.First(x => x != ' ');
+        }
+
+        private int CountTrailingSpaces(string s)
+        {
+            var i = s.LastIndexOf(GetCharInRow(s));
+            return s.Length - i - 1;
+        }
+    }
+}
